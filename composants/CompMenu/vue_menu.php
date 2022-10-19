83b4852e634c16bf15800d91b079bd16fc889e32
isma <ismaelargence3@gmail.com>
2022-10-19 16:55:42 +0200

    debut de la navbar

--- composants/CompMenu/vue_menu.php
+++ composants/CompMenu/vue_menu.php
@@ -4,23 +4,24 @@ class VueMenu {
 
     private $affichageMenu;
 
-    public function __construct () {
-    }
+    public function __construct () {}
 
     public function affiche() {
         echo $this->affichageMenu;
     }
 
     public function menu(){
-        $this->affichageMenu = 
-        '<a href="index.php"><h1>APRES LA PLUIE</h1></a>' .
-        '<a href="index.php?module=co&action=inscription"> Inscription</a></h2>';
+        $this->affichageMenu = '<div class="collapse navbar-collapse"><ul class="navbar-nav">' .
+        '<li class="nav-item active"><a class="nav-link" href="index.php"><img id="logo" class"d-inline-block align-top" src="composants/CompMenu/ressources/logo.png"></a></li>' .
+        '<li class="nav-item active"><a class="nav-link" href="index.php?module=co&action=inscription"> <h3>Inscription</h3></a></li>';
         if (isset($_SESSION['nouvelsession'])){
             $this->affichageMenu = $this->affichageMenu .
-            "<a href=\"index.php?module=co&action=deconnexion\"><h3>deconnexion</h3></a>";
+            "<li class='nav-item active'><a class='nav-link' href=\"index.php?module=co&action=deconnexion\"><h3>deconnexion</h3></a></li>" .
+            "</ul></div>";
         } else {
             $this->affichageMenu = $this->affichageMenu .
-            "<a href=\"index.php?module=co&action=connexion\"><h3>connexion</h3></a>";
+            "<li class='nav-item active'><a class='nav-link' href=\"index.php?module=co&action=connexion\"><h3>connexion</h3></a></li>".
+            "</ul></div>";
         }
     }
 }
