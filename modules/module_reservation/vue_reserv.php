e680d9cdb6425855924dac71f0a782863e36be4a
isma <ismaelargence3@gmail.com>
2022-10-26 16:51:04 +0200

    possibilité de choisir des supplements ouverture d'un modal pour des infos supplementaire

--- modules/module_reservation/vue_reserv.php
+++ modules/module_reservation/vue_reserv.php
@@ -3,9 +3,99 @@
         public function __construct(){parent::__construct();}
 
         public function afficheSupps($tab){
-            foreach ($tab as $key){
-                echo $key['nom'] . " " . $key['quantite'] . " " . $key['description'] . " " . $key['gif_avec'] . " " . $key['gif_sans'] . "<br>";
-            }
+            
+                ?>
+                <div class="container my-5" >
+                    <form action='index.php?module=reserv&action=#' method='post'> <!-- TODO : RAJOUTER UNE ACTION -->
+                        <div class="card">
+                            <!-- header -->
+                            <div class="card-header py-4 px-5 bg-light border-0">
+                                <h4 class="mb-0 fw-bold">Commande ton clip :</h4>
+                            </div>
+                            <!-- body -->
+                            <div class="card-body px-5">
+                                <div class="col-md-4">
+                                    <h5>Supplements : </h5>
+                                    <p class="text-muted">Choisis tes supplmements pour composer ton clip.</p>
+                                </div>
+                                    <?php
+                                    $compt = 1;
+                                        foreach ($tab as $key){
+                                            
+                                            $targetBlaz = "modalVideo";
+                                            $nom = $key['nom'];
+                                            $quantite = $key['quantite'];
+                                            $prix = $key['prix'];
+                                            $description = $key['description'];
+                                            $gifA = $key['gif_avec'];
+                                            $gifS = $key['gif_sans'];
+
+                                            echo '<div class="align border">
+                                                <div class="col-md-6 col-lg-4 mb-4 fullLarg">
+                                                <h3>'. $nom .' </h3>
+                                                <p> quantité : '. $quantite .'</p>
+                                                <p> prix : '. $prix .'€ </p>';
+                                                
+                                            if (!empty($description)){
+                                                ?>
+                                                    <!-- Button trigger modal -->
+                                                    <button type="button" class="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#<?php echo $targetBlaz . "$compt" ?>">
+                                                    Plus d'informations
+                                                    </button>
+
+                                                    <!-- Modal -->
+                                                    <div class="modal fade" id="<?php echo $targetBlaz . "$compt" ?>" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                                        <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+                                                            <div class="modal-content">
+                                                                <div class="modal-header">
+                                                                    <h1 class="modal-title fs-5" id="exampleModalLabel"></h1>
+                                                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                                                </div>
+                                                                <div class="modal-body">
+                                                                    <?php
+                                                                        echo $gifA . $gifS;
+                                                                    ?>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                <?php
+                                                $compt++;
+                                            }
+
+                                            if($nom != "heures-en-plus"){
+                                                echo '<div class="form-check form-switch">
+                                                        <input class="form-check-input" type="checkbox" id="flexSwitchCheckDefault" name="ajoutSupp" value="">
+                                                        <label class="form-check-label" for="flexSwitchCheckDefault">Ajouter à mon clip</label>
+                                                    </div>
+                                                    </div>
+                                                </div>';
+                                            } else {
+                                                echo '
+                                                    <div class="form-floating ">
+                                                        <select class="form-select" id="floatingSelect" aria-label="Floating label select example">
+                                                            <option value="0" selected>pas de temps en plus</option>
+                                                            <option value="1">une heure</option>
+                                                            <option value="2">deux heures</option>
+                                                            <option value="3">trois heures</option>
+                                                        </select>
+                                                        <label for="floatingSelect">choisit le nombres d\'heures</label>
+                                                    </div>
+                                                </div>';
+                                            }
+                                        }
+                                    ?>
+                            </div>
+                            <div class="card-footer text-end py-4 px-5 bg-light border-0">
+                                <button type="submit" class="btn btn-primary btn-rounded">
+                                Commander
+                                </button>
+                            </div>
+                        </div>
+                        
+                    </form>
+                </div>
+            <?php
         }
     }
 ?>
