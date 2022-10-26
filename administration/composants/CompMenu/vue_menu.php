820ad625e3a480118b99ebf853b1668155d45367
nathanfnl <nathan.fnls@gmail.com>
2022-10-26 12:54:58 +0200

    debut du coté admin

--- administration/composants/CompMenu/vue_menu.php
+++ administration/composants/CompMenu/vue_menu.php
@@ -15,16 +15,19 @@ class VueMenu {
         '<li class="nav-item active"><a class="nav-link" href="index.php"><img id="logo" class"d-inline-block align-top" src="composants/CompMenu/ressources/logo.png"></a></li>' .
         '<li class="nav-item active"><a class="nav-link" href="index.php?module=co&action=inscription"> <h3>Inscription</h3></a></li>'.
         '<li class="nav-item active"><a class="nav-link" href="index.php?module=rea&action=afficher_rea"> <h3>Realisations</h3></a></li>' . 
-        '<li class="nav-item active"><a class="nav-link" href="index.php?module=FAQ"> <h3>FAQ</h3></a></li>';
-        if (isset($_SESSION['nouvelsession'])){
-            $this->affichageMenu = $this->affichageMenu .
-            "<li class='nav-item active'><a class='nav-link codeco' href=\"index.php?module=co&action=deconnexion\"><h3>deconnexion</h3></a></li>" .
-            "</ul></div>";
-        } else {
-            $this->affichageMenu = $this->affichageMenu .
-            "<li class='nav-item active'><a class='nav-link codeco' href=\"index.php?module=co&action=connexion\"><h3>connexion</h3></a></li>".
-            "</ul></div>";
-        }
+        '<li class="nav-item active"><a class="nav-link" href="index.php?module=FAQ"> <h3>FAQ</h3></a></li>'.
+        '<li class="nav-item active"><a class="nav-link" href="../index.php"> <h3>UTILISATEUR</h3></a></li>'.
+        '</ul></div>';
+        // if (isset($_SESSION['nouvelsession'])){
+        //     $this->affichageMenu = $this->affichageMenu .
+        //     "<li class='nav-item active'><a class='nav-link codeco' href=\"index.php?module=co&action=deconnexion\"><h3>deconnexion</h3></a></li>" .
+        //     "</ul></div>";
+        // } else {
+        //     $this->affichageMenu = $this->affichageMenu .
+        //     "<li class='nav-item active'><a class='nav-link codeco' href=\"index.php?module=co&action=connexion\"><h3>connexion</h3></a></li>".
+        //     "</ul></div>";
+        // }
+
     }
 }
 ?>
