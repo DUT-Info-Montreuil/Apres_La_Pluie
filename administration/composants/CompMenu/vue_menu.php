f3aced9b84d2aaaf7a54af6595507e8d4612a717
LiberiBg <matheo.ngn@gmail.com>
2022-11-09 16:05:15 +0100

    Clean code NavBar

--- administration/composants/CompMenu/vue_menu.php
+++ administration/composants/CompMenu/vue_menu.php
@@ -19,18 +19,9 @@ class VueMenu {
         <ul class="navbar-nav">' .
         '<li class="nav-item active"><a class="nav-link" href="index.php"><img id="logo" class"d-inline-block align-top" src="composants/CompMenu/ressources/logo.png"></a></li>' .
         '<li class="nav-item active"><a class="nav-link" href="index.php?module=rea&action=afficher_rea"> <h3>Realisations</h3></a></li>' . 
-        '<li class="nav-item active"><a class="nav-link" href="index.php?module=FAQ"> <h3>FAQ</h3></a></li>';
-
-        //Vérification de sécurité si l'utilisateur est toujurs connecté => alors on lui affiche le bouton deconnexion
-         if (isset($_SESSION['nouvelsession'])){
-             $this->affichageMenu = $this->affichageMenu .
-             "<li class='nav-item active'><a class='nav-link codeco' href=\"index.php?module=co&action=deconnexion\"><h3>deconnexion</h3></a></li>";
-         }
-
-         //Chargement du bouton permettant de switch entre l'interface USER et ADMIN
-         $this->affichageMenu = $this->affichageMenu . 
-         '<li class="nav-item active"><a class="nav-link" href="../index.php"> <h3>COTE UTILISATEUR</h3></a></li>
-         </ul></div>';      
+        '<li class="nav-item active"><a class="nav-link" href="index.php?module=FAQ"> <h3>FAQ</h3></a></li>'.
+        '<li class="nav-item active"><a class="nav-link" href="../index.php"> <h3>COTE UTILISATEUR</h3></a></li>
+        </ul></div>';      
     }
 }
 ?>
