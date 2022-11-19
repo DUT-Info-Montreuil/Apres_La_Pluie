ed85f41e6312f8f92f4bf1e5c6ed4ee168391a54
Matheo Nguyen <matheo.ngn@gmail.com>
2022-11-19 15:33:20 +0100

    Modal de changement d'admin ajouté

--- administration/js/fonction-ajax.php
+++ administration/js/fonction-ajax.php
@@ -52,14 +52,37 @@ if(isset($_POST['chercherUtilisateur'])){
             <td>  $col[8]  <a href='' data-bs-toggle='modal' data-bs-target='#modal" . $id . "'><img class ='iconFAQ' src='media/crayon.png' alt='crayon'></a></td>
             </tr>
             ";
+
+            $modal .= "
+            <div class='modal fade' id='modal" . $id . "' tabindex='-1' aria-labelledby='exampleModalLabel' aria-hidden='true'>
+            <div class='modal-dialog'>
+                <div class='modal-content'>
+                    <div class='modal-header'>
+                        <h1 class='modal-title fs-5' id='exampleModalLabel'>ATTENTION</h1>
+                        <button type='button' class='btn-close' data-bs-dismiss='modal' aria-label='Close'></button>
+                    </div>
+                    <div class='modal-body'>
+                        <p>Êtes-vous sûr de vouloir supprimer la question : <br>
+                        </p>
+                    </div>
+                    <div class='modal-footer'>
+                        <button type='button' class='btn btn-secondary' data-bs-dismiss='modal'>Non</button>
+                        <button type='button' class='btn btn-primary'>Oui, supprimer</button>
+                    </div>
+                </div>
+            </div>
+        </div>';
+            
+            
+            ";
         }
 
         $resultat .=
         "</tbody>
         </table>
         ";
-        //CREER MODAL SUPRESSION
 
+        $resultat .= $modal;
 
 
  
