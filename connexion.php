c1daeedbb5d89f3af0e21d314f583f647bc4b4e1
Matheo Nguyen <matheo.ngn@gmail.com>
2022-11-19 17:33:52 +0100

    Modal ok, verification en cours

--- connexion.php
+++ connexion.php
@@ -9,12 +9,9 @@
         }
 
         public static function initConnexion(){
-            // $dsn = 'mysql:dbname=dutinfopw201648;host=database-etudiants.iut.univ-paris8.fr';
-            // $user = 'dutinfopw201648';
-            // $password = 'sypusatu';
-            $dsn = 'mysql:dbname=apres;localhost';
-            $user = 'root';
-            $password = '';
+            $dsn = 'mysql:dbname=dutinfopw201648;host=database-etudiants.iut.univ-paris8.fr';
+            $user = 'dutinfopw201648';
+            $password = 'sypusatu';
 
             self::$bdd = new PDO($dsn, $user, $password, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
         }
