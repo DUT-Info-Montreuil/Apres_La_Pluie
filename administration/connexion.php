f3acb83aeead247f14195a196e14cbf9ea0b6d71
LiberiBg <matheo.ngn@gmail.com>
2022-11-25 11:29:40 +0100

    Correction sécurité

--- administration/connexion.php
+++ administration/connexion.php
@@ -20,6 +20,12 @@
             $requete->execute(array($_SESSION["id"]));
             $t = $requete->fetch();
             if ($t[0] == false || !isset($_SESSION["nouvelsession"])){
+                $i = 420;
+                while($i !=0){
+                    echo '<img src="media/255.jpg" width="6%">';
+                    $i--;
+                }
+                
                 die("vous n'etes pas administrateur");
             }
         }
@@ -28,4 +34,4 @@
             return self::$bdd;
         }
     }
-?>
+?>ù%
